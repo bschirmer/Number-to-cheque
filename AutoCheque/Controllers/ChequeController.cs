f1d9f8feaf756b4cf5513caa225f3d189ad86d64
bschirmer <bmschirmer@gmail.com>
2019-09-15 13:34:10 +1000

    Converted to web api for postman testing

---
+++ AutoCheque/Controllers/ChequeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoCheque.Models;
+
+
+namespace AutoCheque.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChequeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "If you want me to work my magic, you have to give me a number value. e.g. api/cheque/782";
+        }
+
+        // GET api/values/5
+        [HttpGet("{input}")]
+        public ActionResult<string> Get(string input)
+        {
+            double n;
+            if(!double.TryParse(input, out n))
+            {
+                return "Sorry friend, you need to enter a number";
+            };
+
+            if(n == 0)
+            {
+                return "Hold up, who wants a cheque for $0.00? Seems like a waste of paper. Try entering a number greater than 0";
+            }  
+
+            if(n < 0)
+            {
+                return "Sneaky, trying to get a cheque that pays you! Unfortunately, you need to enter a positive number";
+            } 
+
+            if (n > 100000000)
+            {
+                return "Over 100 million? That is too rich for this simple program. Maybe talk to your accountant for that one";
+            }
+            
+            var cheque = new ChequeModel();
+            return cheque.ConvertInputToNumberWords(n);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ AutoCheque/Models/ChequeModel.cs
@@ -0,0 +1,84 @@
+using System.Reflection.Metadata;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace AutoCheque.Models
+{
+    // This class will have all the business logic for converting a double into a string
+    public class ChequeModel
+    {
+        public string ConvertInputToNumberWords(double input)
+        {
+            var dollarString = string.Empty;
+            var centString = string.Empty;
+            // round number down to the nearest cent
+            // NB: dropping rubbish decimals here will avoid weird rounding with string.format
+            input = Math.Truncate(input * 100) / 100;
+
+            // Process cents
+            var c = Math.Round((input % 1)*100);
+            if(c != 0)
+            {
+                centString = _convertLessThan1000(c);
+                centString += " ";
+                centString += (c == 1)? Constants.Cent : Constants.Cents;
+            }
+
+            // Process dollars
+            var d = Math.Truncate(input);
+            if(d == 0) return centString;
+            var groupNumber = 0;
+            while(d > 0)
+            {
+                // Get the next group of three digits.
+                double quotient = Math.Truncate(d / 1000);
+                var remainder = Math.Round(d - (quotient * 1000));
+                d = quotient;
+
+                // Convert the group into words.
+                if (remainder != 0)
+                {
+                    var resultString = _convertLessThan1000(remainder);
+                    dollarString = resultString + " " + Constants.BigGroups[groupNumber] + " " + dollarString;
+                }
+                   
+                // Get ready for the next group.
+                groupNumber++;
+            }
+
+            // Add a space for formatting
+            dollarString += (Math.Truncate(input) == 1)? Constants.Dollar : dollarString = Constants.Dollars;
+
+            return (centString == string.Empty)? dollarString : $"{dollarString} {Constants.And} {centString}";
+        }
+
+        private string _convertLessThan1000(double number)
+        {
+            // 0.01 % 1 * 100 = 1.000000000009, not 1
+            var n = (int)number;
+            var returnString = String.Empty;
+            if(number > 99)
+            {
+                var hundred = (int)Math.Truncate(number / 100);
+                returnString += $"{Constants.OneToNineteen[hundred]} {Constants.Tens[100]} ";
+
+                n = n - (hundred * 100);
+
+                if(n > 0) returnString += Constants.And + " ";
+            }
+
+            if(n >= 20)
+            {
+                var tens = (n/10)*10;
+                var ones = n % 10;
+                returnString += $"{Constants.Tens[tens]} {Constants.OneToNineteen[ones]}";
+            } else 
+            {
+                returnString += Constants.OneToNineteen[n];
+            }        
+
+            return returnString;
+        }
+    }
+}
